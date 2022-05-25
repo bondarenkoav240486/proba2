ce774a7deefc17d05cbf1693cfff91a16de4bb5f
bondarenkoav240486 <bondarenko.av@gmail.com>
2022-05-25 12:51:15 +0300

    commit1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React<br/>
+          react-todo-2
         </a>
       </header>
     </div>
